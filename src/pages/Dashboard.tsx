a7e74cdbe6b4f6118035bb1aa6ba880d178a0b50
Alexander Eckert <eckertalex@protonmail.com>
2020-03-12 22:07:50 +0100

    style: replace CRLF line endings with LF

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Container from 'fragments/Container';
+import { useTranslation } from 'react-i18next';
 /* -------------------------------------------------------------------------- */
 
 const Dashboard: React.FC = () => {

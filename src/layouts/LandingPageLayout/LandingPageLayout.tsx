557fce04300a7dd2b0da8a4bc5d77546e44712a4
Alexander Eckert <eckertalex@protonmail.com>
2020-06-25 21:50:46 +0200

    refactor: Redesign landing page and remove img for svg illustration

--- src/layouts/LandingPageLayout/LandingPageLayout.tsx
+++ src/layouts/LandingPageLayout/LandingPageLayout.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet-async'
 
-import MobileMenu from 'layouts/LandingPageLayout/MobileMenu/MobileMenu'
-import DesktopMenu from 'layouts/LandingPageLayout/DesktopMenu/DesktopMenu'
-import HeroText from 'layouts/LandingPageLayout/HeroText/HeroText'
-import HeroImage from 'layouts/LandingPageLayout/HeroImage/HeroImage'
+import Navbar from 'layouts/LandingPageLayout/Navbar/Navbar'
+import Hero from 'layouts/LandingPageLayout/Hero/Hero'
 import LandingFooter from 'layouts/LandingPageLayout/LandingFooter/LandingFooter'
-import SlantedBar from 'assets/svg/SlantedBar'
 /* -------------------------------------------------------------------------- */
 
 type LandingPageLayoutProps = {
@@ -16,7 +13,6 @@ type LandingPageLayoutProps = {
 }
 
 export default function LandingPageLayout(props: LandingPageLayoutProps) {
-  const [mobileMenuOpen, toggleMobileMenu] = useState(false)
   const { helmetTitle, helmetDescription, children } = props
 
   return (
@@ -25,25 +21,9 @@ export default function LandingPageLayout(props: LandingPageLayoutProps) {
         <title>{helmetTitle}</title>
         <meta name="description" content={helmetDescription} />
       </Helmet>
-      <div className="relative bg-white overflow-hidden">
-        <div className="container mx-auto">
-          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-3xl lg:w-full lg:pb-28 xl:pb-32">
-            <SlantedBar />
-            <DesktopMenu
-              onOpen={() => {
-                toggleMobileMenu(true)
-              }}
-            />
-            <MobileMenu
-              open={mobileMenuOpen}
-              onClose={() => {
-                toggleMobileMenu(false)
-              }}
-            />
-            <HeroText />
-          </div>
-        </div>
-        <HeroImage />
+      <div className="bg-white container mx-auto pb-8">
+        <Navbar />
+        <Hero />
       </div>
       <main className="bg-gray-100">
         <div className="container mx-auto">{children}</div>
